e7d1e867399ee2f1d8ad84cfb36cd4c7643e4432
huangshaoxuan <huangsx@yelinked.com>
2025-12-26 16:46:48 +0800

    feat: 性能指标

--- src/behavior/pv.js
+++ src/behavior/pv.js
@@ -1,5 +1,6 @@
 import { lazyReportBatch } from '../report';
 import { generateUniqueId } from '../utils';
+// page view -- 页面被加载次数（路由切换或页面load时触发）
 export default function pv() {
     const reportData = {
         type: 'behavior',
